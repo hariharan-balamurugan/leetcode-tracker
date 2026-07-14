e333db82cd004524b2af564414a17ddbfb00037c
hariharan-balamurugan <156461632+hariharan-balamurugan@users.noreply.github.com>
2026-07-14 14:02:01 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,12 @@
+// Last updated: 7/14/2026, 2:02:01 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0){
+            return false;
+        }
+        else{
+            return true;
+        }
+        
+    }
+}
