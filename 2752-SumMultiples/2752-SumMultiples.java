44f2ee02bff1591bdaf5b1e078ced1fb6dedb4e7
hariharan-balamurugan <156461632+hariharan-balamurugan@users.noreply.github.com>
2026-07-14 13:57:45 +0530

    Create 2752-SumMultiples

---
+++ 2752-SumMultiples/2752-SumMultiples.java
@@ -0,0 +1,12 @@
+// Last updated: 7/14/2026, 1:57:43 PM
+class Solution {
+    public int sumOfMultiples(int n) {
+        	int sum=0;
+		for(int i=1;i<=n;i++) {
+			if(i%3==0 || i%5==0 || i%7==0) {
+				sum+=i;
+			}
+		}return sum;
+        
+    }
+}
