5d924b1bb5e35c1ef11d14573fd5ebb5311a06a3
hariharan-balamurugan <156461632+hariharan-balamurugan@users.noreply.github.com>
2026-07-14 14:00:27 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,12 @@
+// Last updated: 7/14/2026, 2:00:25 PM
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
