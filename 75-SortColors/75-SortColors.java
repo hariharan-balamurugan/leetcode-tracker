15e5b99bb667af348ad4211e1a5c7ef5e9efb633
hariharan-balamurugan <156461632+hariharan-balamurugan@users.noreply.github.com>
2026-07-14 14:03:26 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.java
@@ -0,0 +1,10 @@
+// Last updated: 7/14/2026, 2:03:26 PM
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i< nums.length;i++){
+            System.out.print(nums[i]);
+        }
+        
+    }
+}
