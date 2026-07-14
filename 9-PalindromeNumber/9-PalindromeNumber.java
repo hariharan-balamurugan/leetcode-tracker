1b5c195b36880a5e592eabc392d91b1b939b6411
hariharan-balamurugan <156461632+hariharan-balamurugan@users.noreply.github.com>
2026-07-14 14:04:23 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,23 @@
+// Last updated: 7/14/2026, 2:04:23 PM
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int n=x;
+        int rev=0;
+        while(n!=0){
+            int last =n%10;
+            rev = rev*10+last;
+            n=n/10;
+        }
+
+        if(rev==x){
+            return true;
+        }
+        return false;
+       
+    }
+
+        
+    }
