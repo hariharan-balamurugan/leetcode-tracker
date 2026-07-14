34a443c82f994115e1c921c19a33e9c1eae280b3
hariharan-balamurugan <156461632+hariharan-balamurugan@users.noreply.github.com>
2026-07-14 13:57:38 +0530

    Create 2955-AccountBalanceAfterRoundedPurchase

---
+++ 2955-AccountBalanceAfterRoundedPurchase/2955-AccountBalanceAfterRoundedPurchase.java
@@ -0,0 +1,8 @@
+// Last updated: 7/14/2026, 1:57:38 PM
+class Solution {
+    public int accountBalanceAfterPurchase(int p) {
+    int n =(( p + 5) / 10 * 10);
+    int a=100-n;
+    return a;
+      }
+    }
