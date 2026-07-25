45899cf116862a9a0ecf5bc53dfad09a83a016a6
hariharan-balamurugan <156461632+hariharan-balamurugan@users.noreply.github.com>
2026-07-25 15:22:25 +0530

    File updated at 7/25/2026, 3:22:27 PM

--- 345-ReverseVowelsofaString/345-ReverseVowelsofaString.java
+++ 345-ReverseVowelsofaString/345-ReverseVowelsofaString.java
@@ -1,45 +1,28 @@
-// Last updated: 7/25/2026, 2:11:04 PM
+// Last updated: 7/25/2026, 3:22:27 PM
 1class Solution {
-2     static boolean isvoewel(char ch){
-3         
-4              if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
-5                  return true;
-6              }
-7          
-8          return false;
-9          
-10      }
-11    public String reverseVowels(String s) {
-12         char[] arr =s.toCharArray();
-13	      int l=0;
-14	      int r=arr.length-1;
-15	      while(l<r){
-16	          if(isvoewel(arr[l])&&isvoewel(arr[r])){
-17	               char  temp =arr[l];
-18	              arr[l]=arr[r];
-19	              arr[r]=temp;
-20	          }
-21	          if(isvoewel(arr[l]) && isvoewel(arr[r])){
-22	              l++;
-23	              r--;
-24	          }
-25	          else if(!isvoewel(arr[l])){
-26	              l++;
-27	              
-28	          }else{
-29	              r--;
-30	          }
-31	          
-32	      }
-33	     String w="";
-34	      for(char num:arr){
-35	          w+=num;
-36	          
-37	      }
-38	      
-39	         
-40
-41      return w;
-42        
-43    }
-44}
+2    public int findMaxConsecutiveOnes(int[] arr) {
+3         int max =0;
+4	      
+5	     int count=0;
+6	      for(int i=0;i<arr.length;i++){
+7	          
+8	          if(arr[i]==1){
+9	              count++;
+10	          }
+11	          else if(arr[i]==0){
+12	              count=0;
+13	          }
+14	          
+15	          max =Math.max(max,count);
+16	          
+17	          
+18	        
+19            
+20	      }
+21	      return max;
+22	      
+23	         
+24
+25        
+26    }
+27}
