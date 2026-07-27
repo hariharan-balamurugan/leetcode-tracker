08aae79b0933e3184d65f9127f616fe2f5093321
hariharan-balamurugan <156461632+hariharan-balamurugan@users.noreply.github.com>
2026-07-27 21:57:18 +0530

    File updated at 7/27/2026, 9:57:16 PM

--- 154-FindMinimuminRotatedSortedArrayII/154-FindMinimuminRotatedSortedArrayII.java
+++ 154-FindMinimuminRotatedSortedArrayII/154-FindMinimuminRotatedSortedArrayII.java
@@ -1,22 +1,38 @@
-// Last updated: 7/27/2026, 9:17:04 PM
+// Last updated: 7/27/2026, 9:57:16 PM
 1class Solution {
-2    public String reversePrefix(String word, char ch) {
-3
-4		int index =0;
-5		for(int i=0;i<word.length();i++){
-6		   index= word.indexOf(ch);
-7		    
-8		    
-9		}
-10		String str =word.substring(0,index+1);
-11		String rev ="";
-12		for(int i=str.length()-1;i>=0;i--){
-13		       rev+=str.charAt(i);
-14		}
-15		String ruslt =rev+word.substring(index+1);
-16        return ruslt;
-17		
-18	}
-19        
-20    }
-21
+2    public boolean strongPasswordCheckerII(String p) {
+3        if(p.length()<8){
+4	      return false;
+5	 }
+6	 boolean lower =false;
+7	 boolean upper =false;
+8	 boolean digit =false;
+9	 boolean special =false;
+10	 String s="!@#$%^&*()-+" ;
+11	 for(int i=0;i<p.length();i++){
+12	     char c =p.charAt(i);
+13	   
+14	     if(Character.isLowerCase(c)){
+15	         lower= true;
+16	     }
+17	     if(Character.isUpperCase(c)){
+18	         upper=true;
+19	     }
+20	     if(Character.isDigit(c)){
+21	         digit= true;
+22	     }
+23	     if(s.contains(String.valueOf(c))){
+24	         special= true;
+25	     }
+26	     if(i>0 && p.charAt(i)==p.charAt(i-1)){
+27	           return false;
+28	         
+29	         
+30	     }
+31	     
+32	 }
+33	 return lower && upper && digit && special;
+34	 
+35        
+36    }
+37}
