f0aec7857fa7f8cbd825440a8e686d29a73c18fb
hariharan-balamurugan <156461632+hariharan-balamurugan@users.noreply.github.com>
2026-07-27 20:56:00 +0530

    File updated at 7/27/2026, 8:55:58 PM

--- 154-FindMinimuminRotatedSortedArrayII/154-FindMinimuminRotatedSortedArrayII.java
+++ 154-FindMinimuminRotatedSortedArrayII/154-FindMinimuminRotatedSortedArrayII.java
@@ -1,8 +1,18 @@
-// Last updated: 7/27/2026, 8:21:26 PM
+// Last updated: 7/27/2026, 8:55:58 PM
 1class Solution {
-2    public double myPow(double x, int n) {
-3        double p=Math.pow(x,n);
-4        return p;
-5        
-6    }
-7}
+2    public long countVowels(String str) {
+3        long s=0;
+4		int n=str.length();
+5		for(int i=0;i<str.length();i++){
+6		    char c = str.charAt(i);
+7             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+8                s += (long)(i + 1) * (n - i);
+9          }
+10		   
+11		
+12		    
+13		}
+14		return s;
+15        
+16    }
+17}
