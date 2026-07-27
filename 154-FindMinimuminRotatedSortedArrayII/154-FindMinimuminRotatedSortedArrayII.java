0f50df91146ee513e9204d2613ebde58d48d2d9f
hariharan-balamurugan <156461632+hariharan-balamurugan@users.noreply.github.com>
2026-07-27 20:21:28 +0530

    File updated at 7/27/2026, 8:21:26 PM

--- 154-FindMinimuminRotatedSortedArrayII/154-FindMinimuminRotatedSortedArrayII.java
+++ 154-FindMinimuminRotatedSortedArrayII/154-FindMinimuminRotatedSortedArrayII.java
@@ -1,13 +1,8 @@
-// Last updated: 7/27/2026, 8:19:34 PM
+// Last updated: 7/27/2026, 8:21:26 PM
 1class Solution {
-2    public int findMin(int[] nums) {
-3        int min =Integer.MAX_VALUE;
-4        for(int i=0;i<nums.length;i++){
-5            if(nums[i]<min){
-6                min=nums[i];
-7            }
-8        }
-9        return min;
-10        
-11    }
-12}
+2    public double myPow(double x, int n) {
+3        double p=Math.pow(x,n);
+4        return p;
+5        
+6    }
+7}
