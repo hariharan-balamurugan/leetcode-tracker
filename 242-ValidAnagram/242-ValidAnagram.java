3cfe15026ebd2bc6f2a2bf36d97aa13151890785
hariharan-balamurugan <156461632+hariharan-balamurugan@users.noreply.github.com>
2026-08-10 14:27:45 +0530

    File updated at 8/10/2026, 2:27:47 PM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,21 +1,20 @@
-// Last updated: 7/14/2026, 2:02:12 PM
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
-            return false;
-        }
-        int[] freq = new int[256];
-        for(int i=0;i<s.length();i++){
-            freq[s.charAt(i)-'a']++;
-            freq[t.charAt(i)-'a']--;
-        }
-        for(int i=0;i<freq.length;i++){
-            if(freq[i]!=0){
-                return false;
-            }
-
-        }
-        return true;
-        
-    }
-}
+// Last updated: 8/10/2026, 2:27:47 PM
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3        if(s.length()!=t.length()){
+4            return false;
+5        }
+6    char [] arr =s.toCharArray();
+7    char[] arr1=t.toCharArray();
+8    Arrays.sort(arr);
+9    Arrays.sort(arr1);
+10    for(int i=0;i<arr.length;i++){
+11        if(arr[i]!=arr1[i]){
+12            return false;
+13        }
+14    }
+15    return true;
+16        
+17        
+18    }
+19}
