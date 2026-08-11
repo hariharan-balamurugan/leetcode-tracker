300bf8658fdca8658bd0f098da75bfecbd03cb5b
hariharan-balamurugan <156461632+hariharan-balamurugan@users.noreply.github.com>
2026-08-11 12:07:06 +0530

    File updated at 8/11/2026, 12:07:07 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -1,25 +1,30 @@
-// Last updated: 8/11/2026, 11:35:08 AM
+// Last updated: 8/11/2026, 12:07:07 PM
 1class Solution {
-2    public String clearDigits(String s) {
-3        Stack<Character>stack =new Stack<>();
-4        String valid="";
-5       
-6        for(int i=0;i<s.length();i++){
-7            char c =s.charAt(i);
-8            if(!Character.isDigit(c)){
-9                stack.push(c);
-10
-11            }
-12            if(Character.isDigit(c) ==Character.isLetter(stack.peek())){
-13                stack.pop();
-14            }
-15
-16        }
-17        while(!stack.isEmpty()){
-18            valid=stack.pop()+valid;
-19        }
-20        return valid;
-21
-22        
-23    }
-24}
+2    public boolean judgeCircle(String moves) {
+3        
+4        int count=0;
+5        int v =0;
+6        for(int i=0;i<moves.length();i++){
+7            char c =moves.charAt(i);
+8            if(c=='U'){
+9                count++;
+10                
+11            }else if(c=='D'){
+12                count--;
+13            }else if(c=='L'){
+14                v++;
+15            }else if(c=='R'){
+16                v--;
+17            }
+18            
+19            
+20              
+21        }
+22        if(count==0 && v==0){
+23            return true;
+24        }
+25        return false;
+26
+27        
+28    }
+29}
