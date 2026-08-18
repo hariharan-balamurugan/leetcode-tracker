6651793174d5ae0e734214d968a41dd2d8b02a76
hariharan-balamurugan <156461632+hariharan-balamurugan@users.noreply.github.com>
2026-08-18 12:08:09 +0530

    File updated at 8/18/2026, 12:08:09 PM

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,37 +1,33 @@
-// Last updated: 7/14/2026, 2:04:08 PM
-class Solution {
-    public boolean isValid(String s) {
-        boolean valid =true;
-        Stack<Character>st =new Stack<>();
-        for(char ch:s.toCharArray()){
-            if(ch=='('||ch=='['||ch=='{'){
-                st.push(ch);
-            }
-            else if(ch==')'||ch==']'||ch=='}'){
-                if(st.isEmpty()){
-                    return false;
-                }
-            
-            if(ch=='}' && st.peek()=='{'){
-                st.pop();
-            }
-            else  if(ch==']' && st.peek()=='['){
-                st.pop();
-            }
-            else if(ch==')' && st.peek()=='('){
-                st.pop();
-            }
-            else{
-                return false;
-            }
-        }
-        }
-        if(st.isEmpty()){
-            return true;
-        }
-    
-    return false;
-        
-        
-    }
-}
+// Last updated: 8/18/2026, 12:08:09 PM
+1class Solution {
+2    public boolean isValid(String str) {
+3        Stack<Character>st =new Stack<>();
+4	//String str ="([])";
+5	for(int i=0;i<str.length();i++){
+6	    char c =str.charAt(i);
+7	    if(c=='('||c=='['||c=='{'){
+8	        st.push(c);
+9	    }
+10        else{
+11            if(st.isEmpty()){
+12                return false;
+13            }
+14               char top=st.peek();
+15	        if(c==')' && top=='('||c==']' && top=='['||c =='}' && top=='{'){
+16	           st.pop();
+17	       }
+18           else{
+19            return false;
+20           }
+21	    
+22	    
+23	}
+24    
+25        
+26    }
+27    return st.isEmpty();
+28    
+29    
+30    //return false;
+31}
+32}
