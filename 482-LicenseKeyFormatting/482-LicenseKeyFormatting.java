36a231e91c466985f75acff4663fc4c3df8d893b
hariharan-balamurugan <156461632+hariharan-balamurugan@users.noreply.github.com>
2026-09-03 16:29:17 +0530

    File updated at 9/3/2026, 4:29:17 PM

--- 482-LicenseKeyFormatting/482-LicenseKeyFormatting.java
+++ 482-LicenseKeyFormatting/482-LicenseKeyFormatting.java
@@ -1,27 +1,28 @@
-// Last updated: 8/6/2026, 11:55:07 AM
+// Last updated: 9/3/2026, 4:29:17 PM
 1class Solution {
 2    public String licenseKeyFormatting(String str, int k) {
-3          Stack<Character>stack = new Stack<>();
-4          String str1="";
-5	    for(char c:str.toCharArray()){
-6	        if(c!='-'){
-7	            stack.push(Character.toUpperCase(c));
-8	        }
-9
-10	    }
-11	    StringBuilder sb =new StringBuilder();
-12	    int count=0;
-13	    while(!stack.isEmpty()){
-14	        sb.append(stack.pop());
-15	        count++;
-16	        
-17	        if(count==k && !stack.isEmpty()){
-18	            sb.append('-');
-19	            count=0;
-20	        }
-21	    }
-22	        str1+= sb.reverse();
-23            return str1;
-24        
-25    }
-26}
+3        StringBuilder sb  =new StringBuilder();
+4        int count=0;
+5        str=str.toUpperCase();
+6        for(int i=str.length()-1;i>=0;i--){
+7            char c =str.charAt(i);
+8            if(c=='-'){
+9                continue;
+10
+11            }
+12            if(count==k){
+13                sb.append('-');
+14                count=0;
+15            }
+16            count++;
+17            sb.append(c);
+18            
+19            
+20
+21        }
+22        return sb.reverse().toString();
+23        
+24         
+25        
+26    }
+27}
