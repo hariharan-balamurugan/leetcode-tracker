b5e6e50300c82a7081c84bc003c46958df277885
Hariharan <156461632+hariharan-balamurugan@users.noreply.github.com>
2026-09-15 12:01:34 +0530

    Create 3940-LimitOccurrencesinSortedArray

---
+++ 3940-LimitOccurrencesinSortedArray/3940-LimitOccurrencesinSortedArray.java
@@ -0,0 +1,13 @@
+// Last updated: 9/15/2026, 12:01:35 PM
+1class Solution {
+2    public int[] limitOccurrences(int[] nums, int k) {
+3    int i=0;
+4    for(int n:nums){
+5        if(i<k ||n!=nums[i-k]){
+6            nums[i++]=n;
+7        }
+8
+9    }
+10    return Arrays.copyOf(nums ,i);
+11    }
+12}
