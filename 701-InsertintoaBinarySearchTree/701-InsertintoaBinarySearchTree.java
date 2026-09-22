d2e70ee475b012849565f2361808b2d53f72ed20
Hariharan <156461632+hariharan-balamurugan@users.noreply.github.com>
2026-09-22 09:45:34 +0530

    Create 701-InsertintoaBinarySearchTree

---
+++ 701-InsertintoaBinarySearchTree/701-InsertintoaBinarySearchTree.java
@@ -0,0 +1,30 @@
+// Last updated: 9/22/2026, 9:45:34 AM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public TreeNode insertIntoBST(TreeNode root, int val) {
+18        if(root==null){
+19            return new TreeNode(val);
+20        }
+21        if(val<root.val){
+22            root.left =insertIntoBST(root.left,val);
+23        }else if(val>root.val){
+24            root.right =insertIntoBST(root.right,val);
+25        }
+26        return root;
+27        
+28    }
+29}
